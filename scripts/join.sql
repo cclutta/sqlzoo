df2f76d716b27fe12d82303848c1ed220fc63b56
Chelsea Lutta <chelsea.lutta@gmail.com>
2024-03-21 14:59:58 +0300

    Joins

--- scripts/join.sql
+++ scripts/join.sql
@@ -27,3 +27,39 @@ WHERE player LIKE 'Mario%';
 
 --5. Show player, teamid, coach, gtime for all goals scored in the first 
 --10 minutes gtime<=10
+SELECT player, teamid, coach, gtime
+FROM goal
+JOIN eteam
+ON goal.teamid = eteam.id
+WHERE gtime <= 10;
+
+
+--6. List the dates of the matches and the name of the team in which 
+--'Fernando Santos' was the team1 coach.
+SELECT mdate, teamname
+FROM game
+JOIN eteam
+ON game.team1=eteam.id
+WHERE coach='Fernando Santos';
+
+--7. List the player for every goal scored in a game where 
+--the stadium was 'National Stadium, Warsaw'
+SELECT player
+FROM goal
+JOIN game
+ON goal.matchid=game.id
+WHERE stadium='National Stadium, Warsaw';
+
+--8. Instead show the name of all players who scored a goal against Germany.
+SELECT DISTINCT player
+  FROM game 
+JOIN goal ON game.id=goal.matchid
+    WHERE teamid <> 'GER'
+AND (team1 = 'GER' OR team2='GER');
+
+--9. Show teamname and the total number of goals scored.
+SELECT teamname, COUNT(teamid)
+FROM eteam
+JOIN goal
+ON eteam.id=goal.teamid
+GROUP BY teamname;
